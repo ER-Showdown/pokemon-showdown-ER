f1ca8771e27b7c74f3755cb186c49a5c935999e3
thekewlstore <thekewlstore@gmail.com>
2024-09-22 10:51:51 -0400

    Including pokemon name in activate text

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -3758,6 +3758,6 @@ export const AbilitiesText: { [k: string]: AbilityText } = {
 	aftershock: {
 		name: "Aftershock",
 		shortDesc: "Triggers Magnitude 4-7 after using a damaging move.",
-		activate: "Created an aftershock!",
+		activate: "  [POKEMON] Created an aftershock!",
 	},
 };
